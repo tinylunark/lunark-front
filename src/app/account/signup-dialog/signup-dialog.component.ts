24f1483c828124f4a05e343528777321548ad660
Momir Milutinović <milutinm462@gmail.com>
2023-11-27 20:45:39 +0100

    feat: Password and password confirmation have to match in sign up form

--- src/app/account/signup-dialog/signup-dialog.component.ts
+++ src/app/account/signup-dialog/signup-dialog.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { AccountService } from '../account.service';
+
+export const passwordConfrimedValidator: ValidatorFn = (
+  control: AbstractControl,
+): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  return password && confirmPassword && password.value != confirmPassword.value
+    ? { passwordConfirmed: false }
+    : null;
+};
 
 @Component({
   selector: 'app-signup-dialog',
@@ -17,14 +28,20 @@ export class SignupDialog {
       Validators.required,
       Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
     ]),
-    password: new FormControl('', [Validators.required]),
-    confirmPassword: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+    ]),
     address: new FormControl('', [Validators.required]),
     phoneNumber: new FormControl('', [
       Validators.required,
       Validators.pattern('[- +()0-9]+')
     ]),
     role: new FormControl('', [Validators.required])
+  }, {
+    validators: passwordConfrimedValidator
   });
 
   constructor(public dialogRef: MatDialogRef<SignupDialog>, private accountService: AccountService) { }
