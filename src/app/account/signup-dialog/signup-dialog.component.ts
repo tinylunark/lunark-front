2d7b1f163160a81d886b47cab3a2b8f00ea08b4b
Momir Milutinović <milutinm462@gmail.com>
2023-11-27 11:26:03 +0100

    feat: Sign up form

--- src/app/account/signup-dialog/signup-dialog.component.ts
+++ src/app/account/signup-dialog/signup-dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-signup-dialog',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class SignupDialog {
 
+  signupForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+    ]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[- +()0-9]+')
+    ]),
+    role: new FormControl('', [Validators.required])
+  });
+
+  constructor(public dialogRef: MatDialogRef<SignupDialog>, private accountService: AccountService) { }
+
+  signUp() {
+    if(this.signupForm.valid) {
+      this.dialogRef.close();
+    }
+  }
 }
