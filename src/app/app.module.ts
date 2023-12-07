f870547bdfdfc6443df2861eab92f7d754816f6b
dusanlecic <dusan.lecic5@gmail.com>
2023-12-07 18:45:50 +0100

    add: missing improts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
@@ -7,7 +8,9 @@ import { LandingModule } from './landing/landing.module';
 import { ProfileModule } from './profile/profile.module';
 import { PropertiesModule } from "./properties/properties.module";
 import { AppRoutingModule } from "./app-routing.module";
-import {FlexLayoutModule} from "@angular/flex-layout";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 
 @NgModule({
@@ -17,13 +20,17 @@ import {FlexLayoutModule} from "@angular/flex-layout";
   imports: [
     LandingModule,
     CoreModule,
+    FormsModule,
+    HttpClientModule,
     ProfileModule,
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
     PropertiesModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
