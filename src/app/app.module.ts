d1236b2a78dc69535938a8e3ab46886fbb139aaf
Momir Milutinović <milutinm462@gmail.com>
2023-12-11 09:32:09 +0100

    fix: Fix duplicate HttpClient identifier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {importProvidersFrom, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
