8de341ef96e5cda92cea25a653f491010f8e3896
Vladislav Radovic <vradovic522@gmail.com>
2023-12-12 14:35:28 +0100

    chore: remove autocomplete

--- src/app/properties/search/search.component.ts
+++ src/app/properties/search/search.component.ts
@@ -11,37 +11,12 @@ import {FilterDialogComponent} from "../filter-dialog/filter-dialog.component";
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  locationControl = new FormControl('');
-  locationOptions: string[] = [];
-  filteredOptions?: Observable<string[]>;
 
   constructor(
     private propertyService: PropertyService,
     private dialog: MatDialog,
   ) {
   }
-
-  // ngOnInit(): void {
-  //   this.getLocations();
-  //
-  //   this.filteredOptions = this.locationControl.valueChanges.pipe(
-  //     startWith(''),
-  //     map(value => this._filter(value || '')),
-  //   );
-  // }
-  //
-  // getLocations(): void {
-  //   this.propertyService.getLocations()
-  //     .subscribe(([]) => this.locationOptions = locations.map(
-  //       (location) => `${location.city}, ${location.country}`
-  //     ));
-  // }
-  //
-  // private _filter(value: string): string[] {
-  //   const filterValue = value.toLowerCase();
-  //
-  //   return this.locationOptions.filter(option => option.toLowerCase().includes(filterValue));
-  // }
 
   openFilterDialog(): void {
     const dialogRef = this.dialog.open(FilterDialogComponent);
