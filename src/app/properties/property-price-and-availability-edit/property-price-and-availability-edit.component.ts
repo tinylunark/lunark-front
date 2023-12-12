3f641ea2f3bf1bbafd883c30784ab63e11bfac4f
Momir Milutinović <milutinm462@gmail.com>
2023-12-12 13:29:38 +0100

    feat: Send price and availability changes to backend

--- src/app/properties/property-price-and-availability-edit/property-price-and-availability-edit.component.ts
+++ src/app/properties/property-price-and-availability-edit/property-price-and-availability-edit.component.ts
@@ -3,6 +3,7 @@ import { DateRange } from '@angular/material/datepicker';
 import { PropertyService } from '../property.service';
 import { ActivatedRoute } from '@angular/router';
 import PropertyAvailabilityEntry from '../../shared/models/property-availability-entry.model';
+import { SharedService } from '../../shared/shared.service';
 
 @Component({
   selector: 'app-property-price-and-availability-edit',
@@ -18,19 +19,20 @@ export class PropertyPriceAndAvailabilityEditComponent implements OnInit {
   autoApproveEnabled: boolean = false;
   cancellationDeadline: number = 0;
   pricingMode: string = 'PER_PERSON';
+  id: number = 0;
 
   dateRange: DateRange<Date> = new DateRange<Date>(this.beginDate, this.endDate);
 
-  constructor(private propertyService: PropertyService, private route: ActivatedRoute) { }
+  constructor(private propertyService: PropertyService, private route: ActivatedRoute, private sharedService: SharedService) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.id = id;
     this.propertyService.getProperty(id).subscribe(property => {
       this.availabilityEntries = property.availabilityEntries;
       this.autoApproveEnabled = property.autoApproveEnabled;
       this.pricingMode = property.pricingMode;
       this.cancellationDeadline = property.cancellationDeadline;
-      console.log(this.availabilityEntries);
     });
   }
 
@@ -42,7 +44,20 @@ export class PropertyPriceAndAvailabilityEditComponent implements OnInit {
   onAvailabilityEntriesAdded(event: PropertyAvailabilityEntry[]): void {
     let availabilityEntryMap: Map<string, PropertyAvailabilityEntry> = new Map<string, PropertyAvailabilityEntry>(this.availabilityEntries.map(entry => [entry.date.toISOString(), entry]));
     event.map(entry => availabilityEntryMap.set(entry.date.toISOString(), entry));
-    console.log(availabilityEntryMap);
-    this.availabilityEntries = Array.from(availabilityEntryMap.values()).sort((a, b) => a.date.getTime() - b.date.getTime());
+  
+    let newAvailabilityEntries = Array.from(availabilityEntryMap.values()).sort((a, b) => a.date.getTime() - b.date.getTime());
+    this.editAvailabilityEntries(newAvailabilityEntries);
+  }
+
+  editAvailabilityEntries(newAvailabilityEntries: PropertyAvailabilityEntry[]): void {
+    this.propertyService.changeAvailability(this.id, newAvailabilityEntries).subscribe({
+      next: (response) => {
+        this.availabilityEntries = response;
+      },
+      error: (error) => {
+        console.log(error);
+        this.sharedService.openSnack("Could not update prices and availability ❌");
+      }
+    })
   }
 }
