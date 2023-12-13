feffaff5d2f767b2d5839e074e29a6290de6f971
Momir Milutinović <milutinm462@gmail.com>
2023-12-13 23:47:51 +0100

    feat: Secure price editing with authGuard

--- src/app/properties/properties-routing.module.ts
+++ src/app/properties/properties-routing.module.ts
@@ -4,10 +4,11 @@ import {PropertiesComponent} from "./properties/properties.component";
 import {PropertyDetailComponent} from "./property-detail/property-detail.component";
 import { SearchPageComponent } from './search-page/search-page.component';
 import { PropertyPriceAndAvailabilityEditComponent } from './property-price-and-availability-edit/property-price-and-availability-edit.component';
+import { authGuard } from '../account/guard/auth.guard';
 
 const routes: Routes = [
   { path: 'properties/:id', component: PropertyDetailComponent },
-  { path: 'properties/:id/edit-price-and-availability', component: PropertyPriceAndAvailabilityEditComponent },
+  { path: 'properties/:id/edit-price-and-availability', component: PropertyPriceAndAvailabilityEditComponent, canActivate: [authGuard], data: { role: ['host'] } },
   { path: 'search', component: SearchPageComponent },
 ];
 
