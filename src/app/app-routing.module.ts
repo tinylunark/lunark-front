c4a7b32764de1cb57c9a6ae80389310209c7bde6
dusanlecic <dusan.lecic5@gmail.com>
2023-12-16 14:22:51 +0100

    add: property approval page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
   { path: 'properties', loadChildren: () => import('./properties/properties.module').then(m => m.PropertiesModule) },
   { path: 'search', loadChildren: () => import('./properties/properties.module').then(m => m.PropertiesModule) },
+  { path: 'approve-changes', loadChildren: () => import('./properties/properties.module').then(m => m.PropertiesModule) },
   { path: '**', redirectTo: '404' },
   { path: '404', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
 ];
