58c25ab2d602aa3b56827f9c091dfa57af255f0e
dusanlecic <dusan.lecic5@gmail.com>
2023-12-16 14:22:14 +0100

    feat: implement some basic property approval page

---
+++ src/app/properties/property-approval/property-approval.component.ts
@@ -0,0 +1,48 @@
+import {Component} from '@angular/core';
+import {PropertyService} from "../property.service";
+import {Property} from "../../shared/models/property.model";
+import {environment} from "../../../env/environment";
+import PropertiesSearchDto from "../properties-search.dto";
+
+@Component({
+  selector: 'app-property-approval',
+  templateUrl: './property-approval.component.html',
+  styleUrl: './property-approval.component.css'
+})
+export class PropertyApprovalComponent {
+  properties: Property[] = [];
+  placeholderImage = environment.assetsDir + '/images/placeholder-image.webp';
+  images: Map<number, string> = new Map;
+  searchDto: PropertiesSearchDto = {
+    amenityIds: [],
+  };
+
+  constructor(private propertyService: PropertyService) {
+  }
+
+  ngOnInit(): void {
+    this.getProperties();
+  }
+
+  getProperties(searchDto?: PropertiesSearchDto): void {
+    this.propertyService.getProperties(searchDto)
+      .subscribe(properties => {
+        this.properties = properties;
+        this.getImages();
+      });
+  }
+
+  /** Gets the first image of each property */
+  getImages(): void {
+    this.properties.forEach(property => {
+      const firstImage = property.images.at(0);
+      if (firstImage) {
+        this.propertyService.getImage(firstImage.id, property.id)
+          .subscribe(image => {
+            const imageUrl = URL.createObjectURL(image);
+            this.images.set(property.id, imageUrl);
+          });
+      }
+    });
+  }
+}
