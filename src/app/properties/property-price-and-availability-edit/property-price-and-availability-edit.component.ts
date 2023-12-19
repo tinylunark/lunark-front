fd94e58a94bb9dcb161a2c065623d58bdff62196
Momir Milutinović <milutinm462@gmail.com>
2023-12-19 19:33:07 +0100

    feat: Upload changes in pricing mode, cancellation deadline and autoapprove to backend

--- src/app/properties/property-price-and-availability-edit/property-price-and-availability-edit.component.ts
+++ src/app/properties/property-price-and-availability-edit/property-price-and-availability-edit.component.ts
@@ -5,29 +5,37 @@ import { ActivatedRoute } from '@angular/router';
 import PropertyAvailabilityEntry from '../../shared/models/property-availability-entry.model';
 import { SharedService } from '../../shared/shared.service';
 import PropertyRequest from '../../shared/models/property-request.model';
+import { convertToPropertyRequest } from '../../shared/models/property.model';
 
 @Component({
   selector: 'app-property-price-and-availability-edit',
   templateUrl: './property-price-and-availability-edit.component.html',
-  styleUrl: './property-price-and-availability-edit.component.css'
+  styleUrl: './property-price-and-availability-edit.component.css',
 })
 export class PropertyPriceAndAvailabilityEditComponent implements OnInit {
   private beginDate: Date = new Date();
   private endDate: Date = new Date();
-  menuItems: string[] = ['Calendar', 'Price Table']
+  menuItems: string[] = ['Calendar', 'Price Table'];
   selectedMenuItem: string = this.menuItems[0];
   id: number = 0;
   property: PropertyRequest | null = null;
 
-  dateRange: DateRange<Date> = new DateRange<Date>(this.beginDate, this.endDate);
+  dateRange: DateRange<Date> = new DateRange<Date>(
+    this.beginDate,
+    this.endDate
+  );
 
-  constructor(private propertyService: PropertyService, private route: ActivatedRoute, private sharedService: SharedService) { }
+  constructor(
+    private propertyService: PropertyService,
+    private route: ActivatedRoute,
+    private sharedService: SharedService
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.id = id;
-    this.propertyService.getProperty(id).subscribe(property => {
-      this.property = property;
+    this.propertyService.getProperty(id).subscribe((property) => {
+      this.property = convertToPropertyRequest(property);
     });
   }
 
@@ -36,23 +44,37 @@ export class PropertyPriceAndAvailabilityEditComponent implements OnInit {
     console.log(item);
   }
 
-  editAvailabilityEntries(newAvailabilityEntries: PropertyAvailabilityEntry[]): void {
-    this.propertyService.changeAvailability(this.id, newAvailabilityEntries).subscribe({
-      next: (response) => {
-        if (this.property == null) {
-          return;
-        }
-        this.property.availabilityEntries = response;
-        this.sharedService.openSnack("Changes saved ✅");
-      },
-      error: (error) => {
-        console.log(error);
-        this.sharedService.openSnack("Could not update prices and availability ❌");
-      }
-    });
+  editAvailabilityEntries(
+    newAvailabilityEntries: PropertyAvailabilityEntry[]
+  ): void {
+    this.propertyService
+      .changeAvailability(this.id, newAvailabilityEntries)
+      .subscribe({
+        next: (response) => {
+          if (this.property == null) {
+            return;
+          }
+          this.property.availabilityEntries = response;
+          this.sharedService.openSnack('Changes saved ✅');
+        },
+        error: (error) => {
+          console.log(error);
+          this.sharedService.openSnack(
+            'Could not update prices and availability ❌'
+          );
+        },
+      });
   }
 
   onSave(): void {
-
+    if (this.property == null) {
+      return;
+    }
+    this.propertyService.updateProperty(this.property).subscribe({
+      next: (response) => {
+        this.property = convertToPropertyRequest(response);
+        this.sharedService.openSnack('Changes saved ✅');
+      },
+    });
   }
 }
