ed9b94507d563f5f61ebfa485e03bc8c09d05160
Vladislav Radovic <vradovic522@gmail.com>
2023-12-26 13:42:17 +0100

    feat: add favorite properties cards

--- src/app/properties/favorites/favorites.component.ts
+++ src/app/properties/favorites/favorites.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {AccountService} from "../../account/account.service";
 import {Property} from "../../shared/models/property.model";
+import {environment} from "../../../env/environment";
+import {PropertyService} from "../property.service";
 
 @Component({
   selector: 'app-favorites',
@@ -9,8 +11,11 @@ import {Property} from "../../shared/models/property.model";
 })
 export class FavoritesComponent {
   favorites?: Property[]
+  placeholderImage = environment.assetsDir + '/images/placeholder-image.webp';
+  images: Map<number, string> = new Map;
 
   constructor(
+    private propertyService: PropertyService,
     private accountService: AccountService,
   ) {
   }
@@ -23,7 +28,22 @@ export class FavoritesComponent {
     this.accountService.getFavoriteProperties()
       .subscribe(favorites => {
         this.favorites = favorites;
-        console.log(favorites);
+        this.getImages();
       });
+  }
+
+  getImages(): void {
+    if (!this.favorites) return;
+
+    this.favorites.forEach(property => {
+      const firstImage = property.images.at(0);
+      if (firstImage) {
+        this.propertyService.getImage(firstImage.id, property.id)
+          .subscribe(image => {
+            const imageUrl = URL.createObjectURL(image);
+            this.images.set(property.id, imageUrl);
+          });
+      }
+    });
   }
 }
