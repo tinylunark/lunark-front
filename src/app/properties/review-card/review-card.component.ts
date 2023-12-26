118da015f218f9198a26efe6e135998445f62f02
Momir Milutinović <milutinm462@gmail.com>
2023-12-26 13:09:21 +0100

    feat: Don't display delete button if the review was not written by the current user

--- src/app/properties/review-card/review-card.component.ts
+++ src/app/properties/review-card/review-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Review } from '../../shared/models/review.model';
+import { AccountService } from '../../account/account.service';
 
 @Component({
   selector: 'app-review-card',
@@ -9,6 +10,13 @@ import { Review } from '../../shared/models/review.model';
 export class ReviewCardComponent {
   @Input() review: Review;
   @Output() deleted: EventEmitter<number> = new EventEmitter<number>();
+  @Output() reported: EventEmitter<number> = new EventEmitter<number>();
+
+  accountService: AccountService;
+
+  constructor(accountService: AccountService) {
+    this.accountService = accountService;
+  }
 
   onDelete(): void {
     this.deleted.emit(this.review.id);
