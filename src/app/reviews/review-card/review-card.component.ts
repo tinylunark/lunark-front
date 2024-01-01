ad05b5dea030c73b9b1023175d9d047ca4287274
Momir Milutinović <milutinm462@gmail.com>
2024-01-01 12:49:10 +0100

    refactor: Move ReviewCardComponent to review module

---
+++ src/app/reviews/review-card/review-card.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Review } from '../../shared/models/review.model';
+import { AccountService } from '../../account/account.service';
+import { environment } from '../../../env/environment';
+import { ApiPaths } from '../../shared/api/api-paths.enum';
+
+@Component({
+  selector: 'app-review-card',
+  templateUrl: './review-card.component.html',
+  styleUrl: './review-card.component.css'
+})
+export class ReviewCardComponent {
+  @Input() review: Review;
+  @Output() deleted: EventEmitter<number> = new EventEmitter<number>();
+  @Output() reported: EventEmitter<number> = new EventEmitter<number>();
+
+  accountService: AccountService;
+
+  constructor(accountService: AccountService) {
+    this.accountService = accountService;
+  }
+
+  onDelete(): void {
+    this.deleted.emit(this.review.id);
+  }
+
+  onReport(): void {
+    this.reported.emit(this.review.id);
+  }
+
+  getProfileImageURL(): string {
+    return `${environment.apiHost}/${ApiPaths.Profile}/${this.review.authorId}/profile-image`;
+  }
+}
