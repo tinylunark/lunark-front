efcac272a147b7f89be6c19082ed9eaca90f830b
dusanlecic <dusan.lecic5@gmail.com>
2024-01-01 21:39:29 +0100

    feat: add routes for reservations

--- src/app/reservations/reservations-routing.module.ts
+++ src/app/reservations/reservations-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { HostIncomingReservationsComponent } from './host-incoming-reservations/host-incoming-reservations.component';
+import { GuestAcceptedReservationsComponent } from './guest-accepted-reservations/guest-accepted-reservations.component';
 import { authGuard } from '../account/guard/auth.guard';
 
 const routes: Routes = [
   { path: 'incoming-reservations', component: HostIncomingReservationsComponent, canActivate: [authGuard], data: { role: ['HOST'] } },
+  { path: 'accepted-reservations', component: GuestAcceptedReservationsComponent, canActivate: [authGuard], data: { role: ['GUEST'] } },
 ];
 
 @NgModule({
