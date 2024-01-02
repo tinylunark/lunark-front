25dbaf9ae267fd77f4d83f5ac9fd437cbf4ed43b
dusanlecic <dusan.lecic5@gmail.com>
2024-01-02 22:20:21 +0100

    update: components patch

--- src/app/reviews/reviews.module.ts
+++ src/app/reviews/reviews.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoreModule } from '../core/core.module';
 import { MaterialModule } from '../../infrastructure/material/material.module';
+import { ReviewRoutingModule } from './reviews-routing.module';
 import { ReviewDialogComponent } from './review-dialog/review-dialog.component';
 import { ConfirmDeleteReviewComponent } from './confirm-delete-review/confirm-delete-review.component';
-
+import { ApproveCommentsGradesComponent } from './approve-comments-grades/approve-comments-grades.component';
+import { ReportedCommentsGradesComponent} from './reported-comments-grades/reported-comments-grades.component';
 
 
 @NgModule({
   declarations: [
     ReviewDialogComponent,
-    ConfirmDeleteReviewComponent
+    ConfirmDeleteReviewComponent,
+    ReportedCommentsGradesComponent,
+    ApproveCommentsGradesComponent,
   ],
   imports: [
     MaterialModule,
-    CommonModule
+    CommonModule,
+    CoreModule,
+    ReviewRoutingModule,
   ],
   exports: [
     ReviewDialogComponent,
-    ConfirmDeleteReviewComponent
+    ConfirmDeleteReviewComponent,
+    ReportedCommentsGradesComponent,
+    ApproveCommentsGradesComponent,
   ]
 })
 export class ReviewsModule { }
