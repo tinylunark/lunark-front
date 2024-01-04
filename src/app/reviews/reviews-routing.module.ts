d8f68054d967be021888fc77aebba583b809c751
dusanlecic <dusan.lecic5@gmail.com>
2024-01-04 20:30:20 +0100

    update: comments & grades route

--- src/app/reviews/reviews-routing.module.ts
+++ src/app/reviews/reviews-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { authGuard } from '../account/guard/auth.guard';
+import { ReportedCommentsGradesComponent } from './reported-comments-grades/reported-comments-grades.component';
 import { ApproveCommentsGradesComponent } from './approve-comments-grades/approve-comments-grades.component';
 
 const routes: Routes = [
+  { path: 'comments-and-grades', component: ReportedCommentsGradesComponent, canActivate: [authGuard], data: {role: ['ADMIN'] } },
   { path: 'unapproved-reviews', component: ApproveCommentsGradesComponent, canActivate: [authGuard], data: {role: ['ADMIN'] } },
 ];
 
