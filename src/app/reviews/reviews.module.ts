2bc93c07dfbc03df5e42e5895ef2b7b828fc34b5
dusanlecic <dusan.lecic5@gmail.com>
2024-01-04 20:30:46 +0100

    update: small fixes

--- src/app/reviews/reviews.module.ts
+++ src/app/reviews/reviews.module.ts
@@ -5,8 +5,8 @@ import { MaterialModule } from '../../infrastructure/material/material.module';
 import { ReviewRoutingModule } from './reviews-routing.module';
 import { ReviewDialogComponent } from './review-dialog/review-dialog.component';
 import { ConfirmDeleteReviewComponent } from './confirm-delete-review/confirm-delete-review.component';
-import { ApproveCommentsGradesComponent } from './approve-comments-grades/approve-comments-grades.component';
 import { ReportedCommentsGradesComponent} from './reported-comments-grades/reported-comments-grades.component';
+import { ApproveCommentsGradesComponent } from './approve-comments-grades/approve-comments-grades.component';
 import { ReviewCardComponent } from './review-card/review-card.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 
---
+++ src/app/shared/models/review-report.model.ts
@@ -0,0 +1,7 @@
+export class ReviewReport {
+  id: number;
+  date: Date;
+  reporterId: number;
+  reviewId: number;
+}
+
