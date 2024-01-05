bd5ab9f8d019e459ec8de5f500967c49cff12470
dusanlecic <dusan.lecic5@gmail.com>
2024-01-05 20:38:47 +0100

    feat: account block component init

---
+++ src/app/account/account-block/account-block.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-block',
+  templateUrl: './account-block.component.html',
+  styleUrl: './account-block.component.css'
+})
+export class AccountBlockComponent {
+
+}
