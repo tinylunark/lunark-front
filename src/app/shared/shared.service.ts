e4a37b59e53941d9cfa4691498ca5c8ea4871d52
Momir Milutinović <milutinm462@gmail.com>
2024-01-05 13:06:01 +0100

    feat: Make snackbar duration adjsutable

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 
+export interface SnackMessage {
+  message: string;
+  duration: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  private snackMessage$ = new BehaviorSubject<string>("");
+  private snackMessage$ = new BehaviorSubject<SnackMessage>({message: "", duration: 3000});
   newSnackMessage = this.snackMessage$.asObservable();
 
   constructor() {}
 
-  openSnack(message: string) {
-    this.snackMessage$.next(message);
+  openSnack(message: string, duration: number = 3000) {
+    this.snackMessage$.next({message: message, duration: duration});
   }
 }
 
