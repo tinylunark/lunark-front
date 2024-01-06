5db86878e7b7c5fb523cca729add851c9f662966
dusanlecic <dusan.lecic5@gmail.com>
2024-01-06 10:28:13 +0100

    add: account block route

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,9 +1,12 @@
-import {RouterModule, Routes} from "@angular/router";
-import {NgModule} from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { authGuard } from './guard/auth.guard';
 import { VerificationComponent } from "./verification/verification.component";
+import { AccountBlockComponent } from './account-block/account-block.component';
 
 const routes: Routes = [
   { path: 'verify/:id', component: VerificationComponent },
+  { path: 'accounts', component: AccountBlockComponent, canActivate: [authGuard], data: {role: ['ADMIN'] } },
 ];
 
 @NgModule({
