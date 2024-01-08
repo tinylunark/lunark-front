9dc4d06ddf4ef6b19b65843cf47ea04f7651c3f8
Vladislav Radovic <vradovic522@gmail.com>
2024-01-08 13:27:21 +0100

    feat: refresh average rating when deleting review

--- src/app/host/host-page/host-page.component.ts
+++ src/app/host/host-page/host-page.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Review } from '../../shared/models/review.model';
-import { ReviewService } from '../../reviews/review.service';
-import { Account } from '../../account/model/account.model';
-import { HostReviewService } from '../../reviews/host-review.service';
-import { AccountService } from '../../account/account.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Review} from '../../shared/models/review.model';
+import {ReviewService} from '../../reviews/review.service';
+import {Account} from '../../account/model/account.model';
+import {HostReviewService} from '../../reviews/host-review.service';
+import {AccountService} from '../../account/account.service';
 
 @Component({
   selector: 'app-host-page',
   templateUrl: './host-page.component.html',
   styleUrl: './host-page.component.css',
-  providers: [{ provide: ReviewService, useClass: HostReviewService }]
+  providers: [{provide: ReviewService, useClass: HostReviewService}]
 })
 export class HostPageComponent implements OnInit {
   reviews: Review[] | null = null;
   id: number | null = null;
   header: string = "";
   reportingAllowed = false;
-  host: Account;
+  averageRating: number;
 
-  constructor(private route: ActivatedRoute, private reviewService: ReviewService, private accountService: AccountService) { }
+  constructor(private route: ActivatedRoute, private reviewService: ReviewService, private accountService: AccountService) {
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -34,10 +35,16 @@ export class HostPageComponent implements OnInit {
         this.reviews = reviews;
       });
 
-      this.accountService.getAccount(this.id).subscribe(host => {
-        this.host = host;
-        this.header = `Host reviews for ${host.name} ${host.surname}`;
-      });
+      this.getAccount();
+    });
+  }
+
+  public getAccount() {
+    if (!this.id) return;
+
+    this.accountService.getAccount(this.id).subscribe(account => {
+      this.averageRating = account.averageRating ?? 0.0;
+      this.header = `Host reviews for ${account.name} ${account.surname}`;
     });
   }
 }
