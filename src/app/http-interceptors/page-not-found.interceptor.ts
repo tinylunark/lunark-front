1bb2253e892beb581c34fa39579baf69a4e4c444
Vladislav Radovic <vradovic522@gmail.com>
2024-01-09 13:22:14 +0100

    Revert "chore: disable 404 interceptor"
    This reverts commit 1e974055f8686e6292ad7cbb47cf51e21d259db9.

--- src/app/http-interceptors/page-not-found.interceptor.ts
+++ src/app/http-interceptors/page-not-found.interceptor.ts
@@ -1,7 +1,9 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse
+} from '@angular/common/http';
 
-import {Observable} from 'rxjs';
+import {catchError, Observable, tap, throwError} from 'rxjs';
 import {Router} from "@angular/router";
 
 /** Redirect to page not found if no resource */
@@ -13,13 +15,12 @@ export class PageNotFoundInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
-      // Disabled for now, causing some issues...
-      // catchError((error: HttpErrorResponse) => {
-        // if (error.status === 404) {
-        //   this.router.navigate(['/404']);
-        // }
-        // return throwError(() => error)
-      // })
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          this.router.navigate(['/404']);
+        }
+        return throwError(() => error)
+      })
     );
   }
 }
