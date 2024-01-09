a876b7609e5138e683afc04daddf994dc2777ef2
Vladislav Radovic <vradovic522@gmail.com>
2024-01-09 19:25:13 +0100

    feat: add notifications enabled toggle

--- src/app/notifications/notifications-page/notifications-page.component.ts
+++ src/app/notifications/notifications-page/notifications-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '../notification.service';
 import { Notification, UnreadNotificationCount, isNotification } from '../../shared/models/notification.model';
+import {MatSlideToggleChange} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-notifications-page',
@@ -9,6 +10,8 @@ import { Notification, UnreadNotificationCount, isNotification } from '../../sha
 })
 export class NotificationsPageComponent implements OnInit {
   notifications: Notification[] = [];
+  notificationsEnabled: boolean;
+
   constructor(private notificationService: NotificationService) { }
   icons: Map<string, string> = new Map<string, string>([
     ['PROPERTY_REVIEW', 'grade'],
@@ -37,6 +40,16 @@ export class NotificationsPageComponent implements OnInit {
         console.log(error);
       }
     });
+
+    this.getNotificationsEnabled();
   }
-  
+
+  getNotificationsEnabled() {
+    this.notificationsEnabled = true;
+  }
+
+  onNotificationsEnabledToggle($event: MatSlideToggleChange) {
+    console.log(`Checked: ${$event.checked}`);
+    this.notificationsEnabled = !this.notificationsEnabled;
+  }
 }
