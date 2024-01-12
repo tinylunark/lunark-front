bf009aa710803b706e11ad79c9d4869f51273ffc
Vladislav Radovic <vradovic522@gmail.com>
2024-01-12 17:49:22 +0100

    feat: add property report form

--- src/app/reports/property-report/property-report.component.ts
+++ src/app/reports/property-report/property-report.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Property} from "../../shared/models/property.model";
+import {PropertyService} from "../../properties/property.service";
+import {AccountService} from "../../account/account.service";
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-property-report',
   templateUrl: './property-report.component.html',
   styleUrl: './property-report.component.css'
 })
-export class PropertyReportComponent {
+export class PropertyReportComponent implements OnInit {
 
+  properties: Property[] = [];
+  form = this.formBuilder.group({
+    year: new FormControl(0, [Validators.required])
+  })
+
+  constructor(
+    private propertyService: PropertyService,
+    private accountService: AccountService,
+    private formBuilder: FormBuilder
+  ) {
+  }
+
+  ngOnInit() {
+    this.getMyProperties();
+  }
+
+  getMyProperties() {
+    const id = this.accountService.getAccountId();
+
+    this.propertyService.getMyProperties(id)
+      .subscribe(result => this.properties = result);
+  }
+
+  onSubmit(propertyId: number) {
+    if (this.form.invalid
+      || !this.form.value.year) return;
+
+    console.log(propertyId, this.form.value.year);
+  }
 }
