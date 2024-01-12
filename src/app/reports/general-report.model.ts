8f34f21f16611bfa73c354fdd4e08bd875b5146e
Vladislav Radovic <vradovic522@gmail.com>
2024-01-12 16:33:09 +0100

    feat: get general report

---
+++ src/app/reports/general-report.model.ts
@@ -0,0 +1,4 @@
+export default interface GeneralReport {
+  reservationCount: number;
+  profit: number;
+}
--- src/app/reports/general-report/general-report.component.ts
+++ src/app/reports/general-report/general-report.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import GeneralReport from "../general-report.model";
+import {ReportService} from "../report.service";
 
 @Component({
   selector: 'app-general-report',
   templateUrl: './general-report.component.html',
   styleUrl: './general-report.component.css'
 })
-export class GeneralReportComponent {
+export class GeneralReportComponent implements OnInit {
 
+  data?: GeneralReport;
+
+  constructor(
+    private reportService: ReportService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getGeneralReport();
+  }
+
+  getGeneralReport() {
+    this.reportService.getGeneralReport(new Date(), new Date())
+      .subscribe(result => this.data = result);
+  }
 }
