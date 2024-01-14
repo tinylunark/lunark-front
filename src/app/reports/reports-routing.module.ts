58ae6942f28e93bf01df114b4659354333647980
Vladislav Radovic <vradovic522@gmail.com>
2024-01-14 16:46:19 +0100

    feat: add auth guard for reports

--- src/app/reports/reports-routing.module.ts
+++ src/app/reports/reports-routing.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {ReportsComponent} from "./reports/reports.component";
+import {authGuard} from "../account/guard/auth.guard";
 
 const routes: Routes = [
-  {path: '', component: ReportsComponent},
+  {path: '', component: ReportsComponent, canActivate: [authGuard], data: {role: ['HOST']}},
 ];
 
 @NgModule({
