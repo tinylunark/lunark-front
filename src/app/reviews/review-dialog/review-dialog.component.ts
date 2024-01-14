3e17497e41a56c442b94bb3e3d400227c386e3aa
Momir Milutinović <milutinm462@gmail.com>
2024-01-14 12:35:06 +0100

    feat: Add validations to the content of a review

--- src/app/reviews/review-dialog/review-dialog.component.ts
+++ src/app/reviews/review-dialog/review-dialog.component.ts
@@ -10,8 +10,8 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class ReviewDialogComponent {
 
   reviewForm: FormGroup = new FormGroup({
-    rating: new FormControl('', Validators.required),
-    description: new FormControl('', Validators.required),
+    rating: new FormControl('', [Validators.required, Validators.min(1), Validators.max(5)]),
+    description: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z0-9 \\.!\\?]+')]),
   });
   rating: number = 0;
   constructor(public dialogRef: MatDialogRef<ReviewDialogComponent>) { }
