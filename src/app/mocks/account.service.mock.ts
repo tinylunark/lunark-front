538ac2f5bd5bc09671055c8034c8d23721b7bc7f
dusanlecic <dusan.lecic5@gmail.com>
2024-01-22 16:08:25 +0100

    add: account service mock update

--- src/app/mocks/account.service.mock.ts
+++ src/app/mocks/account.service.mock.ts
@@ -1,29 +1,35 @@
 import { Injectable } from "@angular/core";
-import { Observable, delay, of, single } from "rxjs";
+import { Observable, delay, of, single, BehaviorSubject } from "rxjs";
 import { Account } from "../account/model/account.model";
 import jasmine from "jasmine";
 
 @Injectable()
 export class AccountServiceMock {
 
-    mockResponse: Account = {
-        id: 1,
-        email: 'test@example.com',
-        password: 'password',
-        name: 'Test User',
-        surname: 'Test Surname',
-        phoneNumber: '123456789',
-        address: 'Test Address',
-        role: 'GUEST'
-    };
+  private user$ = new BehaviorSubject<string | null>(null);
 
-    signUp(account: Account): Observable<Account> {
-        return of(this.mockResponse).pipe(delay(1000));
-    }
+  mockResponse: Account = {
+    id: 1,
+    email: 'test@example.com',
+    password: 'password',
+    name: 'Test User',
+    surname: 'Test Surname',
+    phoneNumber: '123456789',
+    address: 'Test Address',
+    role: 'GUEST'
+  };
+
+  signUp(account: Account): Observable<Account> {
+    return of(this.mockResponse).pipe(delay(1000));
+  }
 
   getProfileImage(): Observable<Blob> {
     const mockImageData = new Uint8Array([1, 2, 3]);
     return of(new Blob([mockImageData], { type: 'image/jpeg' }));
+  }
+
+  setUser(): void {
+    this.user$.next('mockedUser');
   }
 }
 
